1b07b6e9425dc82e64c953db1d25622bf8b6b699
rifqi-qi <ainurrifqi.iqi@gmail.com>
2024-11-06 00:29:34 +0700

    Update app_svd.py

--- app_svd.py
+++ app_svd.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import joblib
+from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 import re
